6fb7301aec55214366ca7bd00b0b4ef952b6ca41
Modukoana22 <119099446+Modukoana22@users.noreply.github.com>
2024-07-31 05:43:35 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,12 @@
+
+package basicframeapp;
+
+import za.ac.tut.ul.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        MyFirstFrame mff = new MyFirstFrame();
+    }
+    
+}
